f867cf60bfcd23788dba67492a3f2604244290c3
Ivan Villa <villaivandev@gmail.com>
2024-06-28 17:02:07 -0300

    primer commit, bases del proyecto

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        // secondary: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#006633",
+        secondary: "#ff6600",
+        oscure: "#141414",
+      },
+    },
+  },
+  plugins: [],
+};
